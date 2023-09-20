3d83abdd8416527c628fb099a96c7b2c8df9b448
polirritmico <ebray187@gmail.com>
2023-09-20 11:39:29 -0300

    Base api_connector done with passed tests

--- tests/test_api_connector.py
+++ tests/test_api_connector.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+
 from src.api_connector import ApiConnector
 
 
-def test_api_connector_headers_validations() -> None:
+def test_headers_validations() -> None:
     api = ApiConnector()
     with pytest.raises(ValueError) as error:
         api._check_headers()
@@ -15,7 +16,7 @@ def test_api_connector_headers_validations() -> None:
     assert expected in output_error
 
 
-def test_api_connector_make_full_url() -> None:
+def test_make_full_url() -> None:
     api = ApiConnector()
     api.base_url = "https://test_url.org"
     endpoint = "/endpoint"
@@ -25,12 +26,29 @@ def test_api_connector_make_full_url() -> None:
     assert expected == output
 
 
-def test_api_connector_make_error_full_url() -> None:
+def test_make_error_full_url() -> None:
     api = ApiConnector()
     api.base_url = ""
     with pytest.raises(ValueError) as error:
         api._make_full_url("/endpoint")
     expected = "without a full url"
-    output_error = str(error.valur.args[0]).lower()
+    output_error = str(error.value.args[0]).lower()
 
+    assert expected in output_error
+
+
+def test_set_headers() -> None:
+    api = ApiConnector()
+    api.set_headers(test_key=123)
+    output = api.headers
+    assert "test_key" in output
+    assert output.get("test_key") == 123
+
+
+def test_set_headers_error() -> None:
+    api = ApiConnector()
+    with pytest.raises(ValueError) as error:
+        api.set_headers()
+    expected = "empty data"
+    output_error = str(error.value.args[0]).lower()
     assert expected in output_error
